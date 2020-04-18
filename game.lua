82991a1341c9da679c0a5f18d74c603ce907f659
Sam Hocevar <sam@hocevar.net>
2020-04-18 22:46:37 +0200

    Reboot project.

--- game.lua
+++ game.lua
@@ -1,65 +1,75 @@
 
-npcs = {}
-cam = vec2(32,32)
+cards = {
+    { name = 'water',
+      cost = 1,
+      desc = 'water plant for 3 turns',
+      turns = 3,
+      effect = function(g) g.water += 1 end
+    },
+    {
+      name = 'sun',
+      cost = 1,
+      desc = 'put the flower in a sunny place',
+      turns = 1,
+      effect = function(g) g.sun = true end,
+    },
+    {
+      name = 'shade',
+      cost = 1,
+      desc = 'put the flower in the shade',
+      effect = function(g) g.sun = false end,
+    },
+}
 
-function new_npc(col)
-    ret = {
-        col = col,
-        pos = vec2(rnd(64), rnd(64)),
+function new_game()
+    local deck = {}
+    for i=1,4 do
+        add(deck, cards[1+rnd(#cards)\1])
+    end
+    return {
+        deck = deck,
+        turn = 1,
+        sun = false,
+        water = 0,
+        light = 0,
+        size = 0,
+
+        sel = -1,
     }
-    return ret
-end
-
-function update_camera()
-    if btn(0) then
-        cam.x = max(cam.x - 0.25, 8)
-    elseif btn(1) then
-        cam.x = min(cam.x + 0.25, 56)
-    end
-
-    if btn(2) then
-        cam.y = max(cam.y - 0.25, 8)
-    elseif btn(3) then
-        cam.y = min(cam.y + 0.25, 56)
-    end
-end
-
-function update_npcs()
-    for npc in all(npcs) do
-        if npc.dest then
-            local d = npc.dest - npc.pos
-            if length(d) < 0.1 then
-                npc.dest = nil
-            else
-                npc.pos += 0.0625 * normalize(d)
-            end
-        else
-            npc.dest = vec2(rnd(64), rnd(64))
-        end
-    end
 end
 
 function _init()
-    for col=1,3 do
-        for i=1,6 do
-            add(npcs, new_npc(col))
-        end
-    end
+    game = new_game()
 end
 
 function _update60()
-    update_camera()
-    update_npcs()
+    if #game.deck > 0 then
+        if game.sel < 0 then
+            game.sel = 1
+        end
+        if btnp(2) then game.sel = 1 + (game.sel - 2) % #game.deck end
+        if btnp(3) then game.sel = 1 + (game.sel) % #game.deck end
+    end
 end
 
 function _draw()
     cls(0)
-    camera(cam.x * 8 - 64, cam.y * 8 - 64)
-    map(0,0,0,0,64,64)
-    for npc in all(npcs) do
-        pal(8, ({8,11,12})[npc.col])
-        spr(8, npc.pos.x * 8, npc.pos.y * 8)
+    color(7)
+    print('turn '..game.turn, 1,1)
+    color(9)
+    print('  water '..game.water, 10, 20)
+    print('  light '..game.light, 10, 27)
+    print('  size '..game.size, 10, 34)
+    print('  sun '..tostr(game.sun), 10, 41)
+    color(12)
+    for i=1,#game.deck do
+        local prefix = i == game.sel and '* ' or '  '
+        print(prefix..game.deck[i].name..' ['..game.deck[i].cost..']', 3, 50 + 7 * i)
     end
-    pal()
+    color(7)
+    if game.sel > 0 then
+        print('description:', 3, 100)
+        print(game.deck[game.sel].desc, 3, 107)
+    end
 end
 
